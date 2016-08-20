ea38837c2cc197baefcd5298967a432ee3192620
Kirsten Land <kirstyland29@gmail.com>
2016-08-20 09:24:06 +0100

    Add basic main and conf files

---
+++ main.lua
@@ -0,0 +1,19 @@
+function love.setup()
+
+end
+
+function love.draw()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.mousepressed(x, y, button, istouch)
+
+end
+
+function love.quit()
+
+end
