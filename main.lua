264ee8c8dedfc05b74c98cf9556a76a319734222
Kirsten Land <kirstyland29@gmail.com>
2016-08-20 09:33:02 +0100

    Basic gamestate code in main

--- main.lua
+++ main.lua
@@ -1,19 +1,37 @@
-function love.setup()
+requires = {"menu", "game"}
 
+for _,j in pairs(requires) do
+  require(j)
+end
+
+local gamestate = menu
+
+function love.setup()
+  gamestate = menu
 end
 
 function love.draw()
-
+  if gamestate.draw then
+    gamestate = gamestate.draw()
+  end
 end
 
 function love.update(dt)
-
+  if gamestate.update then
+    gamestate = gamestate.update(dt)
+  end
 end
 
 function love.mousepressed(x, y, button, istouch)
-
+  if gamestate.mousepressed then
+    gamestate = gamestate.mousepressed(x, y, button, istouch)
+  end
 end
 
 function love.quit()
-
+  local toQuit
+  if gamestate.quit then
+    gamestate, toQuit = gamestate.quit()
+  end
+  return toQuit
 end
